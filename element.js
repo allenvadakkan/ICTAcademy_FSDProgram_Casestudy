5e5f04e425b561050e1d6d40d0e108ee6431b190
Allen <allenjose99@gmail.com>
2020-12-09 21:11:31 +0530

    second commit

--- element.js
+++ element.js
@@ -1,29 +1,89 @@
-function fetchData() {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => {
-      if (!response.ok) {
-        throw Error("ERROR");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const html = data
-        .map((todos) => {
-          return `
-            <tr>
-                <td>${todos.userId}</td>
-                <td>${todos.id}</td>
-                <td>${todos.title}</td>
-                <td><input type="checkbox" id="checkbox" name=${todos.completed}/></td>
-            <tr>`;
-        })
-        .join("");
+//redirect if not valid login
+let currentUser=JSON.parse(window.localStorage.getItem('user'));
+if(!currentUser){
+    window.location='index.html';
 
-      document.querySelector("#todos").insertAdjacentHTML("afterbegin", html);
-    })
-    .catch((error) => {
-      console.log(error);
+}
+
+// setting welcome text
+$('#welcomeText').html(`Hello ${currentUser.uname}! <br> Welcome to your 'WeDo' List`);
+
+// code for log out
+$('#logOut').on('click',(e)=>{
+    e.preventDefault();
+    // Sign-out successful.
+    window.localStorage.removeItem('user');
+    window.location="index.html";
+    console.log('logout successfully');
+})
+
+// AJAX call to fetch data using axios library.
+const getList=async ()=>{
+    try{
+        const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
+        const lists=res.data;
+        console.log(lists);
+        let listcontent='';
+        lists.forEach((el,index)=>{
+            listcontent+=`<li class="list-group-item ${el.completed?'disabledList':''} ${index%2?'list-group-item-info':'list-group-item-success'}"> <input type="checkbox" class="checkbox" ${el.completed?' checked':''}/> <label for=""> ${el.title}</label></li>`
+        });
+        $('#todoList').html(listcontent);
+        if(checkedCount){
+            checkedCount=0;
+        }
+
+    }
+    catch(e){
+        console.log('failed to fetch lists data',e);
+    }
+}
+// call getlist() when GET LIST is clicked.
+$('#getList').on('click',(e)=>{
+    e.preventDefault();
+    getList();
+});
+
+//variable to keep track of cheking list items
+let checkedCount=0;
+
+const alertPromise= ()=>{
+     return new Promise((resolve,reject)=>{
+
+         
+        if(checkedCount===5){
+            resolve(checkedCount)
+        }
+        else{
+            reject('count not equal to 5');
+        }
     });
 }
 
-fetchData();
+const promiseCall=()=>{
+    alertPromise().then((data)=>{
+        alert(`Kudos, We done ${data} activities today.... Congrats!`);
+    })
+    .catch((err)=>{
+        console.log('promise rejected');
+    })
+}
+
+
+getList();
+
+$('#todoList').on('change','.checkbox',function(e){
+    if($(this).prop('checked')===true){
+        console.log('checked');
+        checkedCount++; 
+        $(this).parent().addClass('active');
+    }
+    else{
+        checkedCount--;
+        console.log('unchecked');
+        $(this).parent().removeClass('active');
+    }
+    
+    promiseCall();
+
+
+});
